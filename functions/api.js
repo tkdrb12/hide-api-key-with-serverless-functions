13d8d90d331e01cf0ac1388665a2c3caeb72653f
unknown <tkdrb15@gmail.com>
2023-03-14 16:47:48 +0900

    test1

--- functions/api.js
+++ functions/api.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const querystring = require("querystring");
 const stringify = require("../utils/stringify.js");
 
-const APIS_ORIGIN = "https://api.themoviedb.org/3";
+const APIS_ORIGIN = "https://api.themoviedb.org/3/3";
 const headers = {
   "Access-Control-Allow-Origin": process.env.HOST,
   "Content-Type": "application/json; charset=utf-8",
